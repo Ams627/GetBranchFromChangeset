0dfb6a11726b370557ba8caf191bf47817b7493b
Adrian Sims <asims@parkeon.com>
2017-07-27 17:28:04 +0100

    First Commit

---
+++ GetBranchFromChangeset/LinqExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace GetBranchFromChangeset
+{
+    static class LinqExtensions
+    {
+        public static SortedSet<T> ToSortedSet<T>(this IEnumerable<T> source)
+        {
+            return new SortedSet<T>(source);
+        }
+
+        public static SortedSet<T> ToSortedSet<T>(this IEnumerable<T> source, IComparer<T> comparer)
+        {
+            return new SortedSet<T>(source, comparer);
+        }
+
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source)
+        {
+            return new HashSet<T>(source);
+        }
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer)
+        {
+            return new HashSet<T>(source, comparer);
+        }
+
+        public static void RemoveStrings(this List<string> list, string s)
+        {
+            list.RemoveAll(x => string.Equals(x, s, StringComparison.OrdinalIgnoreCase));
+        }
+        public static void RemoveRegex(this List<string> list, string pattern)
+        {
+            list.RemoveAll(x => Regex.Match(x, pattern, RegexOptions.IgnoreCase).Success);
+        }
+        public static void KeepRegex(this List<string> list, string pattern)
+        {
+            list.RemoveAll(x => !Regex.Match(x, pattern, RegexOptions.IgnoreCase).Success);
+        }
+    }
+}
+
---
+++ GetBranchFromChangeset/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.TeamFoundation.Server;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace GetBranchFromChangeset
+{
+    internal partial class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var name = "http://vstfs2013:8080/tfs/vstfsimport";
+                var uri = new Uri(name);
+                var tfs = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(uri);
+                string path = "$/UK Rail/TVM/Development Heavy Rail/TVM_UK";
+
+                //varChangesetVersionSpec(changeset)
+                var changesetDetails = tfs.GetService<VersionControlServer>().GetChangeset(116361);
+
+                ItemIdentifier rootitem = new ItemIdentifier("$/UK Rail/TVM/Development Heavy Rail/TVM_UK/Source/TVM");
+                BranchObject[] allBranches = tfs.GetService<VersionControlServer>().QueryBranchObjects(rootitem, RecursionType.Full);
+                foreach (var branch in allBranches)
+                {
+                    bool isDeleted = branch.Properties.RootItem.IsDeleted;
+                    Console.Write(isDeleted ? "deleted " : "active ");
+                    Console.WriteLine(branch.Properties.RootItem.Item);
+                }
+
+                var branches = allBranches.Where(x => !x.Properties.RootItem.IsDeleted).Select(y => y.Properties.RootItem.Item).ToHashSet();
+
+                foreach (var change in changesetDetails.Changes)
+                {
+                    var item = change.Item.ServerItem;
+                    foreach (var branch in branches)
+                    {
+                        if (item.Contains(branch))
+                        {
+                            Console.WriteLine(branch);
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
